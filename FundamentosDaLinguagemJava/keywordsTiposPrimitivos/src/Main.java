b602af0cae10e93cbc4cb087a7b7edeef7ac3410
Júlia de Souza <146949046+JuuhSouza@users.noreply.github.com>
2025-08-14 08:24:22 -0300

    Add files via upload

--- FundamentosDaLinguagemJava/keywordsTiposPrimitivos/src/Main.java
+++ FundamentosDaLinguagemJava/keywordsTiposPrimitivos/src/Main.java
@@ -2,6 +2,14 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        
+        byte number = 1;
+        short number2 = 2;
+        int number3 = 3;
+        long number4 = 4;
+        boolean trueOrFlase = true;
+        char number5 = 5;
+        float flutuante = 1;
+        double flutuante2 = 2;
+
     }
 }
